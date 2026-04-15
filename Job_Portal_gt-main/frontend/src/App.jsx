9db1f3a22695fa4190077388ef3b18c7c9cda339
ErGovindthakur <ergovindthakur@gmail.com>
2026-04-16 04:46:05 +0530

    enhance ui

--- Job_Portal_gt-main/frontend/src/App.jsx
+++ Job_Portal_gt-main/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Navbar from './components/shared/Navbar'
+// import Navbar from './components/shared/Navbar'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import Home from './components/Home'
@@ -14,63 +14,69 @@ import AdminJobs from "./components/admin/AdminJobs";
 import PostJob from './components/admin/PostJob'
 import Applicants from './components/admin/Applicants'
 import ProtectedRoute from './components/admin/ProtectedRoute'
+import Layout from './components/Layout'
 
 
 const appRouter = createBrowserRouter([
   {
     path: '/',
-    element: <Home />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/signup',
-    element: <Signup />
-  },
-  {
-    path: "/jobs",
-    element: <Jobs />
-  },
-  {
-    path: "/description/:id",
-    element: <JobDescription />
-  },
-  {
-    path: "/browse",
-    element: <Browse />
-  },
-  {
-    path: "/profile",
-    element: <Profile />
-  },
-  {
-    path:"/admin/companies",
-    element: <ProtectedRoute><Companies/></ProtectedRoute>
-  },
-  {
-    path:"/admin/companies/create",
-    element: <ProtectedRoute><CompanyCreate/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/companies/:id",
-    element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/jobs",
-    element:<ProtectedRoute><AdminJobs/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/jobs/create",
-    element:<ProtectedRoute><PostJob/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/jobs/:id/applicants",
-    element:<ProtectedRoute><Applicants/></ProtectedRoute> 
-  },
-
-])
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/signup',
+        element: <Signup />
+      },
+      {
+        path: "/jobs",
+        element: <Jobs />
+      },
+      {
+        path: "/description/:id",
+        element: <JobDescription />
+      },
+      {
+        path: "/browse",
+        element: <Browse />
+      },
+      {
+        path: "/profile",
+        element: <Profile />
+      },
+      {
+        path:"/admin/companies",
+        element: <ProtectedRoute><Companies/></ProtectedRoute>
+      },
+      {
+        path:"/admin/companies/create",
+        element: <ProtectedRoute><CompanyCreate/></ProtectedRoute> 
+      },
+      {
+        path:"/admin/companies/:id",
+        element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
+      },
+      {
+        path:"/admin/jobs",
+        element:<ProtectedRoute><AdminJobs/></ProtectedRoute> 
+      },
+      {
+        path:"/admin/jobs/create",
+        element:<ProtectedRoute><PostJob/></ProtectedRoute> 
+      },
+      {
+        path:"/admin/jobs/:id/applicants",
+        element:<ProtectedRoute><Applicants/></ProtectedRoute> 
+      },
+    ]
+  }
+]);
 function App() {
 
   return (
